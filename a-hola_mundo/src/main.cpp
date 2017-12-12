585ffc05f50ab537b16d57a7ef7a8042d65fa20b
bqUAB <byron.quezada@e-campus.uab.cat>
2017-12-12 12:19:27 +0100

    Añadí comentarios que explican el programa

--- a-hola_mundo/src/main.cpp
+++ a-hola_mundo/src/main.cpp
@@ -1,12 +1,21 @@
-#include <QApplication>
-#include <QLabel>
+/*******************************************************************************
+ * Mínima aplicación de Qt que simplemente abre una ventana que muestra el texto
+ * Hola Mundo! cuando se ejecuta.
+ ******************************************************************************/
+
+#include <QApplication>  // Necesario para el objeto QApplication
+#include <QLabel>        // Necesario para el objeto QLabel
 
 int main(int argc, char *argv[]) {
 
+  /* QApplication asegura que la aplicación continue ejecutándose hasta que se
+   * cierre su ventana usando un bucle */
   QApplication a(argc, argv);
 
-  QLabel label("Hola Mundo!");
-  label.show();
+  QLabel label("Hola Mundo!");  // Objeto invisible
+  label.show();  // Mostrar el objeto
 
-  return a.exec();
+  return a.exec();  // Inicia el bucle de eventos
+  /* El bucle de eventos termina con la función quit() del objeto QApplication
+   * y sucede indirectamente cuando se cierra la ventana de la aplicación. */
 }
